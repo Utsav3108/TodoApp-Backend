c83aa6d08459710d126e84195251a12790d74818
Utsav Pandya <83863320+Utsav3108@users.noreply.github.com>
2024-06-04 22:13:21 +0530

    Update settings.py Changed Allowed Host

--- TodoAppProject/settings.py
+++ TodoAppProject/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = env('SECRET_KEY', default="asdfasdfasdfasdf")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(" ")
 
 # Password: 123456
 
